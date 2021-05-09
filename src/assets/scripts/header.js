02b65030383e96c680b9dd50f1164d95ce2b8f1f
MrAndre <48051685+MrAndre18@users.noreply.github.com>
2021-05-09 05:58:52 +0300

    Update

--- src/assets/scripts/header.js
+++ src/assets/scripts/header.js
@@ -2,7 +2,7 @@ if ($(".header")) {
   const header = $(".header");
 
   $(window).on("scroll", () => {
-    if ($(window).scrollTop() > 100)
+    if ($(window).scrollTop() > 50)
       header.addClass("scrolled");
     else
       header.removeClass("scrolled");
